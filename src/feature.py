ded73af1016b468ee31afb00d3520300bac056ca
Syed Hussain Ather <shussainather@gmail.com>
2019-09-27 17:33:47 -0700

    sobel

--- src/feature.py
+++ src/feature.py
@@ -9,5 +9,10 @@ Feature extraction
 im = np.zeros((256, 256))
 im[64:-64, 64:-64] = 1
 
-im = ndimage.rotate(im, 15, mode='constant')
+im = ndimage.rotate(im, 15, mode="constant")
 im = ndimage.gaussian_filter(im, 8)
+
+# Sobel gradient operator
+sx = ndimage.sobel(im, axis=0, mode="constant")
+sy = ndimage.sobel(im, axis=1, mode="constant")
+sob = np.hypot(sx, sy)
