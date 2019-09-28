15674bd02c1e6e9c77cfc15c3993eb17535858a2
Syed Hussain Ather <shussainather@gmail.com>
2019-09-27 17:34:20 -0700

    segment

--- src/feature.py
+++ src/feature.py
@@ -16,3 +16,18 @@ im = ndimage.gaussian_filter(im, 8)
 sx = ndimage.sobel(im, axis=0, mode="constant")
 sy = ndimage.sobel(im, axis=1, mode="constant")
 sob = np.hypot(sx, sy)
+
+# Histogram-based segmentation
+n = 10
+l = 256
+im = np.zeros((l, l))
+np.random.seed(1)
+points = l*np.random.random((2, n**2))
+im[(points[0]).astype(np.int), (points[1]).astype(np.int)] = 1
+im = ndimage.gaussian_filter(im, sigma=l/(4.*n))
+mask = (im > im.mean()).astype(np.float)
+mask += 0.1 * im
+img = mask + 0.2*np.random.randn(*mask.shape)
+hist, bin_edges = np.histogram(img, bins=60)
+bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])
+binary_img = img > 0.5
