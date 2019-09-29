e1ebe4dc3253297469c031729dd5bdf41152c43b
Syed Hussain Ather <shussainather@gmail.com>
2019-09-28 21:24:28 -0700

    read image

--- src/hdr.py
+++ src/hdr.py
@@ -1,3 +1,18 @@
+import numpy as np
+
 """
 High dynamic range
 """
+
+def readImagesAndTimes():
+  # List of exposure times
+  times = np.array([ 1/30.0, 0.25, 2.5, 15.0 ], dtype=np.float32)
+   
+  # List of image filenames
+  filenames = ["img_0.033.jpg", "img_0.25.jpg", "img_2.5.jpg", "img_15.jpg"]
+  images = []
+  for filename in filenames:
+    im = cv2.imread(filename)
+    images.append(im)
+   
+  return images, times
