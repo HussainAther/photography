c660b929f4bba08c04093fa42d38464866ff4c6c
Syed Hussain Ather <shussainather@gmail.com>
2019-09-29 21:57:46 -0700

    corner detect

--- src/harris.py
+++ src/harris.py
@@ -12,3 +12,9 @@ cv2.imshow("Image", img)
 # Convert to grayscale
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Grayscale", img_gray)
+
+# Compute Harris corner detector response (params: block size, Sobel aperture, Harris alpha)
+h_response = cv2.cornerHarris(img_gray, 2, 3, 0.04)
+h_min, h_max, _, _ = cv2.minMaxLoc(h_response)  # for thresholding, display scaling
+#print "Harris response: [min, max] = [{}, {}]".format(h_min, h_max)  # [debug]
+cv2.imshow("Harris response", np.uint8((h_response - h_min) * (255.0 / (h_max - h_min))))
