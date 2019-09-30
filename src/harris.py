709b5fca468f40ac6ccd3351f70f5e548a5aef39
Syed Hussain Ather <shussainather@gmail.com>
2019-09-29 21:57:09 -0700

    convert to gray

--- src/harris.py
+++ src/harris.py
@@ -8,3 +8,7 @@ Harris corner detection.
 # Read image
 img = cv2.imread("image/octagon.png")
 cv2.imshow("Image", img)
+
+# Convert to grayscale
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Grayscale", img_gray)
